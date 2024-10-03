2ddcc186da5674738f4ae6cc2907a2ff42019be9
Maicon Ghisleni <144464675+maiconghisleni@users.noreply.github.com>
2024-10-03 19:45:15 -0300

    contat

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var onda1 = document.getElementById('onda1')
 var onda2 = document.getElementById('onda2')
 var onda3 = document.getElementById('onda3')
 var onda4 = document.getElementById('onda4')
+var txtname = document.getElementById('txtnm')
+var txtarea = document.getElementById('txta')
+var txtem = document.getElementById('txtem')
 
 window.addEventListener('scroll', function(){
     var rolagemPos = window.scrollY
@@ -13,17 +16,53 @@ window.addEventListener('scroll', function(){
 })
 
 $(document).ready(
-        function() {
-            $('.masthead').visibility({
-                once: false,
-                 onBottonPassed: function() {
-                    $('.fixed.menu').transition('fade in');
-                },
-                onBottonPassedReverse: function() {
-                    $('.fixed.menu').transition('fade out');
-                }
-            });
+    function() {
+        $('.masthead').visibility({
+            once: false,
+                onBottonPassed: function() {
+                 $('.fixed.menu').transition('fade in');
+            },
+            onBottonPassedReverse: function() {
+                 $('.fixed.menu').transition('fade out');
+            }
+         });
 
-            $('.ui.sidebar').sidebar('attach events','.toc.item');
-        }
-    );
+        $('.ui.sidebar').sidebar('attach events','.toc.item');
+    }
+);
+
+$('document').ready(
+    function() {
+        $('.ui.form').form({
+            fields: {
+                firstname: {
+                    identifier: 'firstname',
+                    rules: [
+                        {
+                            type: 'empty',
+                        }
+                    ]
+                },
+                email: {
+                    indentifier: 'email',
+                    rules: [
+                        {
+                            type: 'empty',
+                        },
+                        {
+                            type: 'email',
+                        }
+                    ]
+                },
+                mensagem: {
+                    identifier: 'mensagem',
+                    rules: [
+                        {
+                            type: 'empty',
+                        }
+                    ]
+                }
+            }
+        })
+    }
+)
